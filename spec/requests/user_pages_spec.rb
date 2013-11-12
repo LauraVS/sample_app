1f8b234261aa2540e9beb7c33cd55e9de2032c97
Laura <lauravs23@hotmail.com>
2013-11-12 13:33:45 +0100

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,9 @@ describe "User pages" do
 
 	subject { page } #  the call to 'should' automatically uses the 'page' variable	
 
+	# -----------------------------------------------------------------------------
 	# Test página de creación de nuevo usuario (SignUp) ---------------------------
+	# -----------------------------------------------------------------------------
 	describe "SignUp page" do
 
 		# antes de cada caso (it) se tiene que visitar signup_path
@@ -15,16 +17,26 @@ describe "User pages" do
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
 
+
 	    describe "with invalid information" do
-	      it "should not create a user" do
-	        expect { click_button buttonText }.not_to change(User, :count)
-	        # Equivalente a:
-	        #  initial = User.count
-	        #  click_button "Create my account"
-	        #  final = User.count
-	        #  expect(initial).to eq final
-	      end
+			it "should not create a user" do
+				expect { click_button buttonText }.not_to change(User, :count)
+				# Equivalente a:
+				#  initial = User.count
+				#  click_button "Create my account"
+				#  final = User.count
+				#  expect(initial).to eq final
+			end
+
+			# Tests for the error messages 
+			describe "after submission" do
+				before { click_button buttonText }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end	      
 	    end	
+
 
 	    describe "with valid information" do
 
@@ -35,15 +47,27 @@ describe "User pages" do
 				fill_in "Email",        with: "user@example.com"
 				fill_in "Password",     with: "foobar"
 				fill_in "Confirmation", with: "foobar"
-			end  	
+			end 
+
 			# Si los datos son válidos, al hacer el submit tiene que haber un usuario más
 			it "should create a user" do
 				expect { click_button buttonText }.to change(User, :count).by(1)
 			end
+
+			describe "after saving the user" do
+				before { click_button buttonText }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') } # have_selector to pick out particular CSS classes along with specific HTML tags.
+			end			
 	    end   	
 	end
 
+
+	# -----------------------------------------------------------------------------
 	# Test página edición de un usuario -------------------------------------------
+	# -----------------------------------------------------------------------------
 	describe "profile page" do
 		# create a User model object with Factory Girl
 		let(:user) { FactoryGirl.create(:user) }
